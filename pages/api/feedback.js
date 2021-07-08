b8b3ca5dea1ee5370385699b667e5c7b5b578d26
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-07-08 08:37:38 +0300

    Using API Routes To Get Data

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -7,6 +7,17 @@ import path from 'path';
 
 // server side code only
 // this code will never exposed to our visitors
+
+function buildFeedbackPath() {
+  return path.join(process.cwd(), 'data', 'feedback.json');
+}
+
+function extractFeedback(filePath) {
+  const fileData = fs.readFileSync(filePath);
+  const data = JSON.parse(fileData);
+  return data;
+}
+
 function handler(req, res) {
   if (req.method === 'POST') {
     const email = req.body.email;
@@ -19,14 +30,15 @@ function handler(req, res) {
     };
 
     // store that in a database or in a file
-    const filePath = path.join(process.cwd(), 'data', 'feedback.json');
-    const fileData = fs.readFileSync(filePath);
-    const data = JSON.parse(fileData);
+    const filePath = buildFeedbackPath();
+    const data = extractFeedback(filePath);
     data.push(newFeedback);
     fs.writeFileSync(filePath, JSON.stringify(data));
     res.status(201).json({ message: 'Success!', feedback: newFeedback });
   } else {
-    res.status(200).json({ message: 'This works!' });
+    const filePath = buildFeedbackPath();
+    const data = extractFeedback(filePath);
+    res.status(200).json({ feedback: data });
   }
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 
 export default function HomePage() {
+
+  const [feedbackItems, setFeedbackItems] = useState([]);
+
   const emailInputRef = useRef();
   const feedbackInputRef = useRef();
 
@@ -25,6 +28,13 @@ export default function HomePage() {
       .then((response) => response.json())
       .then((data) => console.log(data));
   }
+  function loadFeedbackHandler() {
+    fetch('/api/feedback')
+      .then((response) => response.json())
+      .then((data) => {
+        setFeedbackItems(data.feedback);
+      });
+  }
   return (
     <div>
       <h1>Home Page</h1>
@@ -40,6 +50,13 @@ export default function HomePage() {
         </div>
         <button>Send Feedback</button>
       </form>
+      <hr />
+      <button onClick={loadFeedbackHandler}>Load Feedback</button>
+      <ul>
+        {feedbackItems.map((item) => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
     </div>
   )
 }
